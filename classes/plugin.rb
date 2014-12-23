f334c4494931a698479eca3c78795e304efa4379
Alex Stephen <stepa@umich.edu>
2014-12-23 01:09:57 -0500

    Added plugin class

---
+++ classes/plugin.rb
@@ -0,0 +1,19 @@
+module Email
+    class Plugin
+
+        # Used for finding all necessary plugins
+        def self.descendants
+            descendants = []
+            ObjectSpace.each_object(singleton_class) do |k|
+                descendants.unshift k unless k == self
+            end
+            descendants
+        end
+
+        def run(contents)
+            abort_string = '#{self.class} does not have a run method'
+            abort(abort_string)
+        end
+
+    end
+end
